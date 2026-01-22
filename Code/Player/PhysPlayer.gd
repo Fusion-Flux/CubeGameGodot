6a432d382311f524e3efd5d8d23a0cdcb267c815
Fusion-Flux <c4gamer2.0@gmail.com>
2026-01-21 22:24:38 -0600

    more camera changes

--- Code/Player/PhysPlayer.gd
+++ Code/Player/PhysPlayer.gd
@@ -2,8 +2,11 @@ extends RigidBody3D
 
 
 @export var torque_strength = 0.5  # Adjust as needed
+@export var force_strength = 1 
 @export var max_angular_speed = 20.0  # Radians per second
-
+@export var jump_strength = 2.0
+@export var slam_impulse = 50.0
+@export var dash_impulse = 25.0
 # default gravity value (make me maluable! so we can have silly gravity levels and gravity changes)
 @export var gravity = 9.8 
 
@@ -15,11 +18,12 @@ extends RigidBody3D
 
 @export var camera_node = Node3D
 
-@export var checkpoint = Node3D
+@export var camera_controller = Camera3D
+
+@export var checkpoint = Area3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	gravity *= self.mass
-	torque_strength *= self.mass
+	camera_node.global_position = self.global_position
 	pass # Replace with function body.
 
 func get_gravity_direction() -> Vector3:  # Public method
@@ -34,25 +38,30 @@ func set_gravity_direction(direction: Vector3) -> void:  # Public method
 func _process(delta: float) -> void:
 	var isOverspeed = angular_velocity.length() < max_angular_speed;
 	
-	apply_central_force(gravity_direction*gravity*self.mass)
+	apply_central_force(gravity_direction*gravity)
 	var obtained_quat = camera_node.get_quat_no_vert()
 	var obtained_quat_with_vert = camera_node.quaternion
 	var grav_quat = relative_down_node.quaternion
-	if Input.is_action_pressed("rotate_left") and isOverspeed:
-		apply_torque_impulse((grav_quat *(obtained_quat *Vector3.BACK)) * torque_strength)
-	if Input.is_action_pressed("rotate_right") and isOverspeed:
-		apply_torque_impulse((grav_quat *(obtained_quat *Vector3.FORWARD)) * torque_strength)
-	if Input.is_action_pressed("rotate_forward") and isOverspeed:
-		apply_torque_impulse((grav_quat *(obtained_quat *Vector3.LEFT)) * torque_strength)
-	if Input.is_action_pressed("rotate_back") and isOverspeed:
-		apply_torque_impulse((grav_quat *(obtained_quat *Vector3.RIGHT)) * torque_strength)
+	camera_controller.set_camera_fov(self.linear_velocity.length())
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque_impulse(grav_quat * obtained_quat * Vector3.BACK * torque_strength)
+		apply_central_force(grav_quat * obtained_quat * Vector3.LEFT * force_strength)
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque_impulse(grav_quat * obtained_quat * Vector3.FORWARD * torque_strength)
+		apply_central_force(grav_quat * obtained_quat * Vector3.RIGHT * force_strength)
+	if Input.is_action_pressed("rotate_forward"):
+		apply_torque_impulse(grav_quat * obtained_quat * Vector3.LEFT * torque_strength)
+		apply_central_force(grav_quat * obtained_quat * Vector3.FORWARD * force_strength)
+	if Input.is_action_pressed("rotate_back"):
+		apply_torque_impulse(grav_quat * obtained_quat * Vector3.RIGHT * torque_strength)
+		apply_central_force(grav_quat * obtained_quat * Vector3.BACK * force_strength)
 		
 	if Input.is_action_pressed("jump"):
-		apply_impulse((grav_quat *(obtained_quat *Vector3.UP))*self.mass)
+		apply_impulse((grav_quat *(obtained_quat *Vector3.UP))*jump_strength)
 	if Input.is_action_pressed("dash"):
-		apply_impulse((grav_quat *(obtained_quat_with_vert *Vector3.FORWARD))*self.mass)
+		apply_impulse((grav_quat *(obtained_quat_with_vert *Vector3.FORWARD)))
 	if Input.is_action_pressed("slam"):
-		apply_impulse((grav_quat *(obtained_quat *Vector3.DOWN))*self.mass)
+		apply_impulse((grav_quat *(obtained_quat *Vector3.DOWN)))
 	
 	pass
 
