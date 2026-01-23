ddb29e0ec171236eb473c0be6e3ff21079b70eed
Fusion-Flux <c4gamer2.0@gmail.com>
2026-01-23 17:28:22 -0600

    Set up basic prefabs

--- Code/Surfaces/GravSurface.gd
+++ Code/Surfaces/GravSurface.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 
-@export var stored_gravity_direction = Vector3.FORWARD
+@export var stored_gravity_direction = Vector3.DOWN
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
